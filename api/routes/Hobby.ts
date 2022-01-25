a9e7f05303de54790aa36103a877a57e93f036ab
Devin Haynes <dhaynes916@gmail.com>
2022-01-25 07:38:46 -0500

    Added api routes for hobbies

--- api/routes/Hobby.ts
+++ api/routes/Hobby.ts
@@ -10,4 +10,35 @@ router.get("/", (req, res) => {
     .catch((e) => res.status(400).json(e));
 });
 
+// DELETE /hobbies
+// Delete hobby
+router.delete("/:id", async (req, res) => {
+  const id = req.params.id;
+  await HobbyModel.deleteOne({ _id: id })
+    .then(() => res.json({ success: true }))
+    .catch((e) => res.status(400).json(e));
+});
+
+// GET /hobbies/:id
+// Get single hobby from db
+router.get("/:id", async (req, res) => {
+  await HobbyModel.findOne({ _id: req.params.id })
+    .then((hobby) => {
+      res.json(hobby);
+    })
+    .catch((e) => {
+      res.status(400).json(e);
+    });
+});
+
+// POST /hobbies
+// Post a new hobby
+router.post("/", (req, res) => {
+  const hobby = new HobbyModel({
+    hobby: req.body.hobby,
+  });
+
+  hobby.save((e: any) => (e ? res.json("Didn't work") : res.json("Worked")));
+});
+
 module.exports = router;
